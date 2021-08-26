b691d414f2fd05c4b348a4bb525390ff940417fc
Axiean <axieans@gmail.com>
2021-08-26 14:01:59 +0430

    import imgs and start header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <h2>hii</h2>
+    <div>
+      <Home />
     </div>
   );
 }
