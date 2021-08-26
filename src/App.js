58bb276b5a0dde23260b803f957df4ddc79e5edc
Axiean <axieans@gmail.com>
2021-08-26 13:25:27 +0430

    starting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Aug , 26 13:30 just started my new Front Mentor Challenge</p>
+        <h5>wu wins...</h5>
         <a
           className="App-link"
           href="https://reactjs.org"
