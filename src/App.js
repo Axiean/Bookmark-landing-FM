7f6cd1974d332b29b18ce2f0731a60064dc73921
Axiean <axieans@gmail.com>
2021-08-26 13:40:02 +0430

    config to start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Aug , 26 13:30 just started my new Front Mentor Challenge</p>
-        <h5>wu wins...</h5>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>hii</h2>
     </div>
   );
 }
